b66dfe74672c1fff214b2c03f5bb2efb5eacc266
Rob <39972327+mechlink@users.noreply.github.com>
2020-04-03 15:55:29 +1000

    Add files via upload

---
+++ HeapAccessor.cpp
@@ -0,0 +1,42 @@
+//
+// Sams Teach yourself C++ in 24 Hours, 6th Edition
+// Accessing Data Members using Pointers
+// Listing 11.2 page 157
+// 3rd April 2020
+//
+#include <iostream>
+using namespace std;
+
+
+class SimpleCat
+{
+public:
+    SimpleCat() { itsAge = 2; }
+    ~SimpleCat() {}
+    int GetAge() const { return itsAge; } // const never change in value
+    void SetAge(int age) { itsAge = age; }
+private:
+    int itsAge;
+};
+
+int main()
+{
+    SimpleCat *Frisky = new SimpleCat;
+    cout << "Frisky is " << Frisky->GetAge()
+              << " years old" << "\n";
+
+    Frisky->SetAge(5);
+    cout << "Frisky is " << Frisky->GetAge()
+              << " years old\n";
+
+    Frisky->SetAge(9);
+    cout << "Frisky is " << Frisky->GetAge()
+              << " years old\n";
+
+
+    delete Frisky;
+
+    return 0;
+}
+
+
